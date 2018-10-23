3a0bb978a2aa591667a48ba1f1ca00a79e7473ea
shensheng <shensheng@31huiyi.com>
2018-10-23 14:40:13 +0800

    zhong

---
+++ app/scripts/mainDetail.js
@@ -0,0 +1,137 @@
+/*!
+ *
+ *  Web Starter Kit
+ *  Copyright 2015 Google Inc. All rights reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License
+ *
+ */
+/* eslint-env browser */
+(function () {
+  'use strict';
+
+  // Check to make sure service workers are supported in the current browser,
+  // and that the current page is accessed from a secure origin. Using a
+  // service worker from an insecure origin will trigger JS console errors. See
+  // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
+  var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+  );
+
+  if ('serviceWorker' in navigator &&
+    (window.location.protocol === 'https:' || isLocalhost)) {
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function (registration) {
+        // updatefound is fired if service-worker.js changes.
+        registration.onupdatefound = function () {
+          // updatefound is also fired the very first time the SW is installed,
+          // and there's no need to prompt for a reload at that point.
+          // So check here to see if the page is already controlled,
+          // i.e. whether there's an existing service worker.
+          if (navigator.serviceWorker.controller) {
+            // The updatefound event implies that registration.installing is set:
+            // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
+            var installingWorker = registration.installing;
+
+            installingWorker.onstatechange = function () {
+              switch (installingWorker.state) {
+                case 'installed':
+                  // At this point, the old content will have been purged and the
+                  // fresh content will have been added to the cache.
+                  // It's the perfect time to display a "New content is
+                  // available; please refresh." message in the page's interface.
+                  break;
+
+                case 'redundant':
+                  throw new Error('The installing ' +
+                    'service worker became redundant.');
+
+                default:
+                // Ignore
+              }
+            };
+          }
+        };
+      }).catch(function (e) {
+      console.error('Error during service worker registration:', e);
+    });
+  }
+
+  // Your custom JavaScript goes here
+})();
+(function ($) {
+  $(function () {
+
+    var browser={
+      versions:function(){
+        var u = navigator.userAgent, app = navigator.appVersion;
+        return {//移动终端浏览器版本信息
+          trident: u.indexOf('Trident') > -1, //IE内核
+          presto: u.indexOf('Presto') > -1, //opera内核
+          webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+          gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+          mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+          ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+          android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或者uc浏览器
+          iPhone: u.indexOf('iPhone') > -1 , //是否为iPhone或者QQHD浏览器
+          iPad: u.indexOf('iPad') > -1, //是否iPad
+          webApp: u.indexOf('Safari') == -1, //是否web应该程序，没有头部与底部
+          weixin: u.indexOf('MicroMessenger') > -1, //是否微信
+          qq: u.match(/\sQQ/i) == " qq" //是否QQ
+        };
+      }(),
+      language:(navigator.browserLanguage || navigator.language).toLowerCase()
+    }
+    function isPhone() {
+      return browser.versions.mobile || browser.versions.ios || browser.versions.android || browser.versions.iPhone || browser.versions.iPad
+    }
+    // const isPhone = () => true
+    var handler = function (hash) {
+      var target = document.getElementById(hash.slice(1));
+      if (!target) return;
+      $('.headNav a').removeClass('active');
+      $('.headNav a[data-href='+hash+']').addClass('active');
+      var targetOffset = $(target).offset().top - window.innerWidth/100*4.5;
+      $('html,body').animate({scrollTop: targetOffset}, 400);
+      if (isPhone()) {
+        $('.sidenav').sidenav('close')
+      }
+    }
+
+    $('a[data-href^=#][data-href!=#]').click(function () {
+      location.href= './index.html'
+      // setTimeout(() => {
+      //   handler($(this).attr('data-href'))
+      // }, 500)
+    });
+
+    if(isPhone()){
+      // 手机环境
+      $('.sidenav').sidenav({
+        edge: 'right'
+      });
+      $('#slideOut').click(function() {
+        $('.sidenav').sidenav('open')
+      })
+      $('#closesidenav').click(function() {
+        $('.sidenav').sidenav('close')
+      })
+    }
+
+  });
+})(jQuery);
